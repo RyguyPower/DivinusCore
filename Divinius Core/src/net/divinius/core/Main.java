c8625a844a042f961e7ee46da095867edf38ff27
Ryguy Power <49324839+RyguyPower@users.noreply.github.com>
2019-11-02 20:37:09 -0700

    First Commit

---
+++ Divinius Core/src/net/divinius/core/Main.java
@@ -0,0 +1,19 @@
+package net.divinius.core;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin{ 
+	
+	@Override
+    public void onEnable() {
+        // Plugin startup logic
+
+
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
